7d27177d810db7478baf0fa63c5c6f75d7070f40
ChenjieJia <70700692+ChenjieJia@users.noreply.github.com>
2021-04-23 22:21:55 +0800

    Create BPlusTreeString.h

---
+++ Code/BPlusTreeString.h
@@ -0,0 +1,43 @@
+#ifndef BPLUSTREESTRING_H
+#define BPLUSTREESTRING_H
+
+#include "BPlusNnode.h"
+#include <vector>
+using namespace std;
+ 
+enum COMPARE_OPERATOR{LT, LE, EQ, BE, BT, BETWEEN}; // 比较操作符：<、<=、=、>=、>、<>
+const int INVALID_INDEX = -1;
+ 
+struct SelectResult
+{
+	int keyIndex;
+	CLeafNode* targetNode;
+};
+ 
+class BPlusTreeString{
+public:
+	BPlusTreeString();
+	BPlusTreeString(const string& filename); // load 100Mb file of text
+	~BPlusTreeString();
+	char getByte(const int& position) const; // get the kth byte
+	string getString(const int& m, const int& n) const; // get a string from byte m to n
+	void replaceString(const int& m, const int & n); // replace a string from byte m to n
+	void insertOneByte(const char& c, const int& position); // inserting 1 byte in the middle of the file (offset=50 million for example)
+	void insertString(const string& str, const int& position); // inserting 10 bytes, 100 bytes, 1k bytes 10k bytes (normal string)
+	void insertString(const BPlusTreeString& filename, const int& position); // inserting 10 bytes, 100 bytes, 1k bytes 10k bytes (ropelike)
+	void endAdd(const string& str); // appending to end of file 1, 10, 100, 1k, 10k bytes.(normal string)
+	void endAdd(const BPlusTreeString& filename); // appending to end of file 1, 10, 100, 1k, 10k bytes.(ropelike)
+	void remove(const int& position, const int& k); // deleting 1, 10, 100, 1k, 10k bytes and the entire file.(normal string)
+	void output() const; // saving the entire text to a file
+
+private:
+	int numBytes; // total number of bytes
+	interNode* baseNode;
+	leafNode* start;
+	leafNode* finish;
+};
+
+BPlusTreeString::BPlusTreeString(const string& filename) {}
+
+
+#endif
